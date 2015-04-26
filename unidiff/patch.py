d9bdd7ff35ed383c782fdb9d0d8a072e80e3608f
David Baum <serjantik@gmail.com>
2015-04-25 20:01:50 -0400

    Replaced the added and removed lines arrays variables with the string

--- unidiff/patch.py
+++ unidiff/patch.py
@@ -164,8 +164,8 @@ class PatchedFile(list):
         self.source_timestamp = source_timestamp
         self.target_file = target
         self.target_timestamp = target_timestamp
-	self.added_lines = []
-	self.removed_lines = []
+	self.added_lines_content = ""
+	self.removed_lines_content = ""
 
     def __repr__(self):
         return make_str("<PatchedFile: %s>") % make_str(self.path)
@@ -198,11 +198,11 @@ class PatchedFile(list):
             if line_type == LINE_TYPE_ADDED:
                 original_line.target_line_no = target_line_no
                 target_line_no += 1
-		self.added_lines.append("{0}\n".format(value))
+		self.added_lines_content = "{0}\n{1}".format(self.add_lines_content, value)
             elif line_type == LINE_TYPE_REMOVED:
                 original_line.source_line_no = source_line_no
                 source_line_no += 1
-		self.removed_lines.append("{0}\n".format(value))
+		self.removed_lines = "{0}\n{1}".format(self.removed_lines_content, value)
             elif line_type == LINE_TYPE_CONTEXT:
                 original_line.target_line_no = target_line_no
                 target_line_no += 1
